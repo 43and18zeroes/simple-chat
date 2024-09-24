b52b42f2c77e0cf54b9f6955d822b5041ed6e328
Christoph <c.wagner.germany@gmail.com>
2024-09-24 17:54:29 +0200

    dj9

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Message
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'author', 'receiver')
+
 # Register your models here.
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
