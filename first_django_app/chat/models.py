87ef106c800fe2220f0762f1324e6c10efa762bd
Christoph <c.wagner.germany@gmail.com>
2024-09-24 16:20:13 +0200

    dj4

--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db import DateField
+from datetime import date
 
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = DateField(default=date.today)
