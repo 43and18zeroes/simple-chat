f454fa338a6eed18fbd8e48825de0a47c6e77386
Christoph <c.wagner.germany@gmail.com>
2024-10-01 13:44:36 +0200

    py1

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -3,24 +3,48 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from .models import Chat, Message
+from django.contrib.auth.models import User
+
 
 # Create your views here.
-@login_required(login_url='/login/')
+@login_required(login_url="/login/")
 def index(request):
-    if request.method == 'POST':
-        print("Received data " + request.POST['textmessage'])
+    if request.method == "POST":
+        print("Received data " + request.POST["textmessage"])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        Message.objects.create(
+            text=request.POST["textmessage"],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user,
+        )
     chatMessages = Message.objects.filter(chat__id=1)
-    return render(request, 'chat/index.html', {'messages': chatMessages})
+    return render(request, "chat/index.html", {"messages": chatMessages})
+
 
 def login_view(request):
-    redirect = request.GET.get('next')
-    if request.method == 'POST':
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+    redirect = request.GET.get("next")
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST.get("username"), password=request.POST.get("password")
+        )
         if user:
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            return HttpResponseRedirect(request.POST.get("redirect"))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
-    return render(request, 'auth/login.html', {'redirect': redirect})
+            return render(
+                request,
+                "auth/login.html",
+                {"wrongPassword": True, "redirect": redirect},
+            )
+    return render(request, "auth/login.html", {"redirect": redirect})
+
+
+def register_view(request):
+    # if request.method == "POST":
+        # user = User.objects.create_user(
+        #     username="john", email="jlennon@beatles.com", password="glass onion"
+        # )
+    # print("link")
+    # print(user)
+    return render(request, "auth/register.html")
